fd3e80e87dca3ee11851aac3ef1193d468cf76ca
SaiAshwathaSingari <singarisai777@gamil.com>
2025-10-25 01:00:49 +0530

    Deploy pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/e-plantShopping/' // <-- your repo name here
+  base: '/e-plantShopping/'  // <-- must match your repo name exactly
 });
